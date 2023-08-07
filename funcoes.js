8975ba4e22e04a6ff3788b3a4d4aafad8671f586
Sandro Alencar Fernandes <sandro@arealocal.com.br>
2023-08-07 14:28:26 -0300

    Correção do FOR

--- funcoes.js
+++ funcoes.js
@@ -1,6 +1,6 @@
 /* função parinpar() retorna se é um numero par ou ímpar. */
 function parimpar(numero) {
-   if ((numero % 2) === 0) {
+   if ((numero % 2) == 0) {
       //número par.
       return "par";
    } else {
@@ -11,8 +11,10 @@ function parimpar(numero) {
 /* percorrer de 1 até determinado número e testa se é par ou ímpar */
 function geraPares(ate) {
     const numeros = document.querySelector("#pNumeros");
-    for (x=1;x++;x <= ate) {
-        if (parimpar(x) === "par") {
+    numeros.innerHTML = ""; //limpar o paragrafo de números
+    for (x=1;x <= ate;x++) {
+        console.log(x);
+        if (parimpar(x) == "par") {
             numeros.innerHTML += x + "<br>";
             /* numeros.innerHTML += equivale a numeros.innerHTML = numeros.innerHTML + */
         }
